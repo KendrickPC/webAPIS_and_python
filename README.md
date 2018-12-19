6a72cfae82e65cece6e342600eac4ef6cf2707d1
KendrickPC <kennethpchang@gmail.com>
2018-12-19 19:55:55 +0800

    git_repos.py bar chart created.

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 
 https://api.github.com/search/repositories?q=language:python&sort=stars
 
+##### Github rate limit check
+https://api.github.com/rate_limit
+
+
+
 <!-- Useful Command Line Checks for Pip -->
 python -m ensurepip
--- git_repos.py
+++ git_repos.py
@@ -1,17 +1,3 @@
-# import requests
-
-# # Make an API call and store the response.
-# url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
-# r = requests.get(url)
-# print("Status code:", r.status_code)
-
-# # Store API response in a variable.
-# response_dict = r.json()
-
-# # Process results.
-# print(response_dict.keys())
-
-
 import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
@@ -24,3 +10,47 @@ print("Status code:", r.status_code)
 # Store API response in a variable.
 response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+
+names, stars = [], []
+
+# print("Repositories return:", len(repo_dicts))
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+# print("\nKeys:", len(repo_dict))
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+# print("\nSelected information about each repository:")
+# for repo_dict in repo_dicts:
+#     print('Name:', repo_dict['name'])
+#     print('Owner:', repo_dict['owner']['login'])
+#     print('Stars:', repo_dict['stargazers_count'])
+#     print('Repositories:', repo_dict['html_url'])
+#     print('Created:', repo_dict['created_at'])
+#     print('Updated:', repo_dict['updated_at'])
+#     print('Description:', repo_dict['description'])
+
+# Make visualization
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('github_repos.svg')
